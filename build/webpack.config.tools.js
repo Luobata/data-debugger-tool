96d40b35e19da4083dc20d8ef6e9bf1daf6d6a96
yaochenghao <yaochenghao@bytedance.com>
2018-04-19 20:58:44 +0800

    init: webpack config

--- build/webpack.config.tools.js
+++ build/webpack.config.tools.js
@@ -1,24 +1,49 @@
-import path from 'path'
-import webpack from 'webpack'
+import path from 'path';
+import webpack from 'webpack';
+import vueLoaderConfig from './vue-loader.conf.js';
 
 export default {
     entry: {
         background: './src/background',
     },
     output: {
-        path: path.join(__dirname, 'build'),
-            filename: '[name].js'
+        path: path.join(__dirname, '/dev-tools/build'),
+        filename: '[name].js',
     },
-        resolve: {
-            extensions: ['.ts', '.js', '.vue', '.json'],
+    resolve: {
+        extensions: ['.ts', '.js', '.vue', '.json'],
         alias: {
             SRC: path.resolve(__dirname, '../src'),
             COMPONENTS: path.resolve(__dirname, '../src/components'),
-            LIB: path.resolve(__dirname, '../src/lib')
-        }
+            LIB: path.resolve(__dirname, '../src/lib'),
+        },
     },
     module: {
-        rules: [],
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: vueLoaderConfig(),
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    cacheDirectory: true,
+                },
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: utils.assetsPath('img/[name].[hash:7].[ext]'),
+                },
+            },
+        ],
     },
-    
-},
+};
